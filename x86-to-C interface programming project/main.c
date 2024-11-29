e9f8c46998ec3b282fba6adf5cf5bb9322a465c4
chiarajugno <jugnochiara@gmail.com>
2024-11-29 22:29:13 +0800

    added c and asm files

--- x86-to-C interface programming project/main.c
+++ x86-to-C interface programming project/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <math.h>
+
+
 
 int main() {
+
+	//int vectorSizes[] = { (1 << 20), (1 << 24), (1 << 30) };
+	int n = 2;
+	double A[] = {2.0, 1.0};
+	double B[] = {2.0, 1.0};
+	//double sdot = 0.0;
 
 
 	return 0;
