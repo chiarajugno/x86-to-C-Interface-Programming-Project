a1d165596cdeecaf97eee733cf2f6e340db953d2
kathleentrc <kathleen_cruz@dlsu.edu.ph>
2024-11-30 00:02:27 +0800

    updated code

---
+++ x86-to-C interface programming project/kernelAssembly.asm
@@ -0,0 +1,23 @@
+section .data
+ans dq 0.0
+
+section .text
+bits 64
+default rel
+global callAssembly
+
+callAssembly:
+    movsd xmm3, [ans]
+L1:
+    movsd xmm1, [rdx]
+    movsd xmm2, [r8]
+    
+    mulsd xmm1, xmm2
+    addsd xmm3, xmm1
+    
+    add rdx, 8
+    add r8, 8
+    loop L1
+    
+    movsd [r9], xmm3
+    ret 
--- x86-to-C interface programming project/main.c
+++ x86-to-C interface programming project/main.c
@@ -3,17 +3,43 @@
 #include <windows.h>
 #include <math.h>
 
+extern void callAssembly(unsigned long long int n, double* x, double* y, double* sdot);
 
+void callC(unsigned long long int n, double* x, double* y, double* sdot) {
+	for (int i = 0; i < n; i++) {
+		*sdot += x[i] * y[i];
+	}
+}
 
 int main() {
+	unsigned long long int n = 1 << 30;
+	unsigned long long int bytes = n * sizeof(double);
 
-	//int vectorSizes[] = { (1 << 20), (1 << 24), (1 << 30) };
-	int n = 2;
-	double A[] = {2.0, 1.0};
-	double B[] = {2.0, 1.0};
-	//double sdot = 0.0;
-	// test push
+	double* x; 
+	double* y;
+	double sdot = 0.0;
 
+	x = (double*)malloc(bytes);
+	y = (double*)malloc(bytes);
+
+	// initialize
+	for (int i = 0; i < n; i++) {
+		x[i] = 2.0;
+		y[i] = 1.0;
+	}
+
+	callC(n, x, y, &sdot);
+	printf("answer is %lf\n", sdot);
+
+	// reinitialize
+	sdot = 0.0;
+	for (int i = 0; i < n; i++) {
+		x[i] = 2.0;
+		y[i] = 1.0;
+	}
+
+	callAssembly(n, x, y, &sdot);
+	printf("answer is %lf", sdot);
 
 	return 0;
 }
